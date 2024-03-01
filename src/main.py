dc83547972809ac8e4d91ab73717714d3e11ae7e
jay santana <jayannyrayara@gmail.com>
2024-03-01 12:08:42 -0300

    T002: feat - Implementação e criação da lógica do jogo para o jogador

--- src/main.py
+++ src/main.py
@@ -1,4 +1,15 @@
-print("Olá, mundo!")
+import random
 
-msg = "Hello World"
-print(msg)
+numero_aleatorio = random.randint(1, 100)
+print(numero_aleatorio)
+
+while True:
+    palpite_humano = int(input("Digite um número entre 1 e 100: "))
+
+    if palpite_humano == numero_aleatorio:
+        print("Parabéns!! Você acertou o número!")
+        break
+    elif palpite_humano < numero_aleatorio:
+        print("O número é maior, tente novamente!")
+    else:
+        print("O número é menor, tente novamente!")
