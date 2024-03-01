f8b12db7ba0e81a9ac73cb2b41e9f82142034594
jay santana <jayannyrayara@gmail.com>
2024-03-01 12:16:44 -0300

    T002: feat - Implementação e criação da lógica do jogo para o computador

--- src/main.py
+++ src/main.py
@@ -4,12 +4,26 @@ numero_aleatorio = random.randint(1, 100)
 print(numero_aleatorio)
 
 while True:
+    # Vez do jogador
     palpite_humano = int(input("Digite um número entre 1 e 100: "))
 
     if palpite_humano == numero_aleatorio:
-        print("Parabéns!! Você acertou o número!")
+        print(f"Parabéns!! Você acertou o número! O número secreto era: {numero_aleatorio}")
         break
     elif palpite_humano < numero_aleatorio:
         print("O número é maior, tente novamente!")
     else:
         print("O número é menor, tente novamente!")
+    
+    # Vez do computador
+    palpite_computador = random.randint(1, 100)
+   
+    print(f"O computador palpita: {palpite_computador}")
+
+    if palpite_computador == numero_aleatorio:
+      print(f"O computador acertou o número! O número secreto era: {numero_aleatorio}")
+      break
+    elif palpite_computador < numero_aleatorio:
+      print("O palpite do computador é menor.")
+    else:
+      print("O palpite do computador é maior.")
