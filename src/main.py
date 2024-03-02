b9d56ecdb9afa6badff96da63e070aeb93171c70
jay santana <jayannyrayara@gmail.com>
2024-03-02 09:46:04 -0300

    T002: feat - Implementação da lógica para registro das suposições dos jogadores

--- src/main.py
+++ src/main.py
@@ -3,9 +3,13 @@ import random
 numero_aleatorio = random.randint(1, 100)
 print(numero_aleatorio)
 
+suposicoes_jogador = []
+suposicoes_computador = []
+
 while True:
     # Vez do jogador
     palpite_humano = int(input("Digite um número entre 1 e 100: "))
+    suposicoes_jogador.append(palpite_humano)
 
     if palpite_humano == numero_aleatorio:
         print(f"Parabéns!! Você acertou o número! O número secreto era: {numero_aleatorio}")
@@ -17,6 +21,7 @@ while True:
     
     # Vez do computador
     palpite_computador = random.randint(1, 100)
+    suposicoes_computador.append(palpite_computador)
    
     print(f"O computador palpita: {palpite_computador}")
 
@@ -27,3 +32,6 @@ while True:
       print("O palpite do computador é menor.")
     else:
       print("O palpite do computador é maior.")
+    
+    print("\nSuposições do jogador:", suposicoes_jogador)
+    print("Suposições do computador:", suposicoes_computador)
