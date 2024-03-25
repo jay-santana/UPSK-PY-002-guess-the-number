38c4affaf09cf609df5fae0d1461a2f1d04fd415
jay santana <jayannyrayara@gmail.com>
2024-03-25 18:21:37 -0300

    T007: refactor(testes): Refatoração do código de testes com inserção de docstrings, quebra de linhas longas e correção de escrita na verificação usando is True ou is False

--- src/main_test.py
+++ src/main_test.py
@@ -1,60 +1,82 @@
-from main_module import computer_turn, player_turn, play_again, display_guesses, check_winner
+"""
+Este módulo contém testes para as funções do seu programa principal.
+"""
 from unittest.mock import patch
+from main_module import computer_turn, player_turn, play_again, display_guesses, check_winner
 
-# Testes para função jogar novamente
-# Verifica se a função retorna True quando o usuário insere 's'
+# Testes para função play_again
 def test_play_again_yes():
-    # Simula entrada válida 's'
-    with patch('builtins.input', return_value='s'):
-        assert play_again() == True
+    """
+    Testa se a função play_again retorna True quando o usuário insere 's'.
+    """
+    with patch('builtins.input', return_value='s'): # Simula entrada válida 's'
+        assert play_again() is True
 
-# Verifica se a função retorna False quando o usuário insere 'n'
 def test_play_again_no():
-    # Simula entrada válida 'n'
-    with patch('builtins.input', return_value='n'):
-        assert play_again() == False
+    """
+    Testa se a função play_again retorna False quando o usuário insere 'n'.
+    """
+    with patch('builtins.input', return_value='n'): # Simula entrada válida 'n'
+        assert play_again() is False
 
-# Verifica se a função solicita uma entrada válida novamente ao receber um valor inválido
 def test_play_again_invalid_input_then_yes():
+    """
+    Testa se a função play_again solicita uma entrada válida novamente ao receber um valor inválido
+    e retorna True quando o usuário insere 's' após a entrada inválida.
+    """
+    with patch('builtins.input', side_effect=['x', 's']):
     # Simula entrada inválida 'x' e então entrada válida 's'
-    with patch('builtins.input', side_effect=['x', 's']): 
-        assert play_again() == True
+        assert play_again() is True
 
 #Teste para função computer_turn
 def test_computer_turn():
-    # verifique se o palpite do computador está dentro do intervalo fornecido
+    """
+    Testa a função computer_turn para garantir que o palpite do computador
+    esteja dentro do intervalo fornecido.
+    """
     low, high = 1, 100
     guess = computer_turn(low, high)
     assert low <= guess <= high
 
-# Testa se a função player_turn retorna um número válido dentro do intervalo
+# Teste para função player_turn
 def test_player_turn(monkeypatch):
-    # Simula a entrada do usuário
-    monkeypatch.setattr('builtins.input', lambda _: '42')  
+    """
+    Testa se a função player_turn retorna um número válido dentro do intervalo
+    após simular a entrada do usuário.
+    """
+    monkeypatch.setattr('builtins.input', lambda _: '42') # Simula a entrada do usuário
     result = player_turn("NomeDoJogador")
     assert 1 <= result <= 100
 
-# Teste para a função de exibir suposições dos jogadores
+# Teste para função display_guesses
 def test_display_guesses():
-    # Define os palpites dos jogadores
+    """
+    Testa a função display_guesses para verificar se a saída contém o nome do jogador
+    e seus palpites.
+    """
     player_guesses = [1, 2, 3]
     computer_guesses = [10, 20, 30]
     player_name = "Jogador1"
 
-    # Verifica se a saída contém o nome do jogador e seus palpites
-    assert display_guesses(player_name, player_guesses) == f"Suposições do Jogador, {player_name}: {player_guesses}"
-    assert display_guesses("Computador", computer_guesses) == f"Suposições do Jogador, Computador: {computer_guesses}"
+    assert display_guesses(player_name, player_guesses) == (
+        f"Suposições do Jogador, {player_name}: {player_guesses}"
+        )
+    assert display_guesses("Computador", computer_guesses) == (
+        f"Suposições do Jogador, Computador: {computer_guesses}"
+        )
 
 # Teste para a função check_winner
 def test_check_winner():
-    # Verifica se a função retorna True quando o palpite é igual ao número secreto
-    assert check_winner("NomeDoJogador", 42, 42, [10, 28, 42], 3) == True
-
-    # Verifica se a função retorna False quando o palpite é menor que o número secreto
-    assert check_winner("NomeDoJogador", 42, 30, [20, 25, 30], 3) == False
-
-    # Verifica se a função retorna False quando o palpite é maior que o número secreto
-    assert check_winner("NomeDoJogador", 42, 50, [45, 48, 50], 5) == False
+    """
+    Testa a função check_winner para verificar se retorna True quando o palpite é igual 
+    ao número secreto, e False quando o palpite é menor ou maior que o número secreto.
+    """
+    assert check_winner(player_name="NomeDoJogador", secret_number=42,
+                        guess=42, guesses=[10, 28, 42], attempts=3) is True
+    assert check_winner(player_name="NomeDoJogador", secret_number=42,
+                        guess=30, guesses=[20, 25, 30], attempts=3) is False
+    assert check_winner(player_name="NomeDoJogador", secret_number=42,
+                        guess=50, guesses=[45, 48, 50], attempts=3) is False
 
 
 
